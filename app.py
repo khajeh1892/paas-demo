892c35c467626ab93f9cc63772c53eeb048bac9d
khajeh2001 <khajehaminianneda2001@gmail.com>
2025-12-17 21:49:04 +0330

    revert to simple working app

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
-import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", build=datetime.datetime.utcnow().isoformat())
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
