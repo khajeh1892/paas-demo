989b5f227f1317202ee7f0021a581d8c26628a36
khajeh2001 <khajehaminianneda2001@gmail.com>
2025-12-17 20:10:02 +0330

    add root route

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def home():
     return "PaaS app is running successfully 🚀"
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/health")
+def health():
+    return {"status": "ok"}
