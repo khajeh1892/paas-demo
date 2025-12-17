90d2f864ecee3fbb13ff63ed93bb1ab9df27f6bf
khajeh2001 <khajehaminianneda2001@gmail.com>
2025-12-17 21:13:18 +0330

    debug build stamp

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", build=datetime.datetime.utcnow().isoformat())
